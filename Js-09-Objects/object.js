6fe21767ae6d20921882796766bb21c4ac5e9347
Ferhat Yigit <yiitferhat@gmail.com>
2024-12-17 16:10:48 +0300

    commit

--- Js-09-Objects/object.js
+++ Js-09-Objects/object.js
@@ -104,8 +104,166 @@ console.log(this)
 console.log(personel.calculateAge())
 console.log(personel.arrowFunction())
 
- //! Arrow functionda this window objesine karşılık gelir. Arrow functionda this kullanmıyoruz
-  //! window objesi içinde isMarried isimli bir özellik bulamadığından undefined
+//! Arrow functionda this window objesine karşılık gelir. Arrow functionda this kullanmıyoruz
+//! window objesi içinde isMarried isimli bir özellik bulamadığından undefined
 console.clear()
-    /* -------------------------------------------------------------------------- */
-  //* ------- OKUMA/ERİŞME ----------
+/* -------------------------------------------------------------------------- */
+//* ------- OKUMA/ERİŞME ----------
+
+//! 1- .(dot) notation ile erişim
+console.log(personel.name)
+console.log(personel.birth)
+console.log(personel.children.forEach((child) => console.log(child)))
+console.log(personel.horoscope)
+console.log(personel.address.city)
+console.log(personel.address.mahalle)
+console.log(personel.selamla())
+
+
+//! 1- [] square brackets, özellik yöntemi
+console.log(personel["name"]);
+console.log(personel["horoscope"]);
+console.log(personel["address"]["city"]);
+
+//dinamik değerlere erişim için avantajlıdır.
+
+const keyWord = "birth"
+console.log(personel[keyWord]);
+console.clear();
+
+/* -------------------------------------------------------------------------- */
+
+//? yeni bir özellik (property) ekleme
+personel.phone = "342-456-246"
+personel[keyWord] = 1990
+console.log(personel);
+
+/* -------------------------------------------------------------------------- */
+console.log(`Personel: ${personel.name}`);
+
+//? Bir objeyi kopyalama
+//! 1- Shallow copy (sığ) kopyalama
+personel.name = "Hatice"
+const firmaPersoneli = personel
+firmaPersoneli.name = "Fatma"
+console.log(`Personel: ${personel.name}`);
+console.log(`firmaPersoneli: ${firmaPersoneli.name}`);
+//! 2- Deep Copy yöntemi 
+// JSON.stringify() , JSON.parse()
+console.log("****************-*-*********");
+const deepCopyPersonel = JSON.parse(JSON.stringify(personel))
+console.log(deepCopyPersonel);
+deepCopyPersonel.name = "Zehra"
+console.log(personel);
+console.log(deepCopyPersonel);
+
+
+//! 3- Object Assign    
+//! 4- Spread Operatoru
+console.clear();
+
+//* ------------------------------------------------------ */
+//*                     OBJECT METHODS
+//* ------------------------------------------------------ */
+
+//? İçice (nested) object kullanımı
+
+const employeeList = {
+    person1: { name: "Salih", lastname: "sina", age: 45 },
+    person2: { name: "sema", lastname: "semrügül", age: 34 },
+    person3: { name: "Nida", lastname: "nehaber", age: 24 },
+    person4: { name: "Fena", lastname: "fin", age: 35 },
+}
+
+console.log(employeeList.person2.lastname);
+console.log(employeeList["person2"]["lastname"]);
+
+//* -------------------------------------------------------- */
+//*                   FOR - OF -IN YAPISI                    */
+//* -------------------------------------------------------- */
+
+//? for ... of 
+//! for ... of sıralı olan dizilerde kullanılır ama objectlerde daha komple bir yapı olduğundan yapı olduğu için for of kullanılmaz.
+
+// for (let i of employeeList){
+//     console.log(i);
+// }
+
+//? for ... in 
+//! for ... in yapısı ile obje keyleri üzerinde döngü kurabiliriz ancak bu tavsiye edilen bir yöntem değildir. Bunun yerine obje özelliklerini diziye dönüştüren metodları kullanabiliriz.
+
+for (i in employeeList) {
+    console.log(i);
+}
+
+for (i in personel) {
+    console.log(i);
+}
+
+console.clear();
+/* -------------------------------------------------------------------------- */
+// for in yada for of yerine object arraye çeviren yöntemler var 
+// bir object ; key - value çiftlerinden oluşur.
+
+//? KEYlere erişim sağlamak için object.keys metodu kullanılır.
+const keys = Object.keys(personel)
+console.log(keys);
+
+//? VALUE lara erişim için object.values()
+
+const value = Object.values(personel)
+console.log(value);
+
+//? OBJECT.ENTRIES
+
+const degerler = Object.entries(personel)
+console.log(degerler);
+
+
+/* ------------------------------------------------------ */
+//*         JSON -Javascript Script Object Notation       */
+//* ------------------------------------------------------ */
+console.clear()
+//? JSON ile farklı veri türleri barındırılabilir.
+//? Dillerden bağımsızdır.
+//? nesnelerin (objelerin) temsil edilmesi için anahtar-değer çiftlerini kullanır. Bu, verilerin daha iyi organize edilmesini ve ilişkilendirilmesini sağlar.
+//? JSON, veri  işleme açısından hızlıdır. Bu nedenle web uygulamalarında ve servislerde yaygın olarak kullanılır.
+
+const employees = [
+    {
+        name1: "Abdulkadir",
+        lastName: "baki",
+        dateOfBirth: 1980,
+        salary: 20000,
+        job: "developer",
+    },
+    {
+        name2: "elif",
+        lastName: "akalın",
+        dateOfBirth: 1990,
+        salary: 20000,
+        job: "developer",
+    },
+    {
+        name3: "esra",
+        lastName: "bilgin",
+        dateOfBirth: 1985,
+        salary: 21000,
+        job: "devops",
+    },
+];
+
+employees.map((employee) => console.log(employee.salary))
+// bir array olduğu için yeni bir eleman eklemek için array metodlarından push kullanılır
+
+employees.push(
+    {
+        name4: "Celal",
+        lastName: "Baki",
+        dateOfBirth: 1980,
+        salary: 50000,
+        job: "developer",
+    }
+)
+
+console.log(employees);
